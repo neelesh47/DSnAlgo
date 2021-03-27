27d85ab1c1eb8e50e4889926ba4ea5e45ff0b23c
neelesh47 <neil.lufc10@gmail.com>
2021-03-27 18:02:23 +0530

    pair sum

---
+++ pairsum.py
@@ -0,0 +1,14 @@
+def pair_sum(arr, k):
+    if len(arr)<2:
+        return 
+    seen = set()
+    output = set()
+    for num in arr:
+        target = k - num
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add((min(num,target),max(num,target)))
+    return output
+
+print(pair_sum( [-5, 1, -40, 20, 6, 8, 7 ],15))
